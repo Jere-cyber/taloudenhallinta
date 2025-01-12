872d51292991d6d2f39b0e8b7f32993f8559e6ee
Jere Vikstedt <koodaajaguru@gmail.com>
2025-01-12 16:44:20 +0200

    lisää EditItem-komponentin, reitityksen ja tietojen välityksen

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -5,6 +5,7 @@ import Root from '../Root'
 import Settings from '../Settings'
 import Stats from '../Stats'
 import AddItem from '../AddItem'
+import EditItem from '../EditItem'
 
 
 
@@ -18,6 +19,17 @@ function AppRouter(props) {
         { path: "", 
           element: <Items />, 
           loader: () => { return props.data } },
+          { path: "edit/:id",
+            element: <EditItem onItemSubmit={props.onItemSubmit} />,
+            loader: ({params}) => {
+              const item = props.data.filter(item => item.id === params.id).shift()
+              if (item) {
+                return { item }
+              } else {
+                throw new Response("Not Found", { status: 404 })
+              }
+            } },
+  
         { path: "stats", element: <Stats /> },
         { path: "settings", element: <Settings /> },
         { path: "add", element: <AddItem onItemSubmit={props.onItemSubmit} /> },
