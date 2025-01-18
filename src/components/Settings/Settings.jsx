fd1cfaa27d9930f262733f740d2db81c667a196b
Jere Vikstedt <koodaajaguru@gmail.com>
2025-01-18 13:17:34 +0200

    lisää setting sivun ulkoasujen muotoilut

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -3,16 +3,17 @@ import styles from './Settings.module.scss'
 function Settings(props) {
 
   return (
-    <div className={styles.Settings}>
+    <div className={styles.settings}>
       <h2>Asetukset</h2>
-      <h3>kulutyypit</h3>
-      <div className={styles.Settings}>
+      <h3>Kulutyypit</h3>
+      <div className={styles.settings_types}>
         { props.typelist.map(
-          type=> <div key={type}>{type}</div>
+            type => <div key={type}>{type}</div>
         )}
-    </div>
+      </div>
     </div>
   )
+
 }
 
 export default Settings
