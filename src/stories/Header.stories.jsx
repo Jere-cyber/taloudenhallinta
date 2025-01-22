ec47e6d84e67bcc17e5236298aa5c6482249e10f
Jere Vikstedt <koodaajaguru@gmail.com>
2025-01-22 22:10:58 +0200

    lisää Storybook-dokumentaation

---
+++ src/stories/Header.stories.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+
+
+
+export default {
+    title: 'Components/Header',
+    component: Header,
+    tags: ['autodocs'],
+  }
+  export const Default = { }
